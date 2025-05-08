8bd0a7fd3c2560168c8e7a53a5aecdd02b01cc46
DeveloperRooney <han_0427@naver.com>
2025-05-08 16:01:20 +0900

    work

---
+++ src/main/kotlin/com/back/domain/member/member/entity/Member.kt
@@ -0,0 +1,21 @@
+package com.back.domain.member.member.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class Member(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    @Column(nullable = false, unique = true)
+    val username: String,
+    @Column(nullable = false)
+    var password: String,
+    @Column(nullable = false)
+    var name: String,
+) {
+}
--- src/test/kotlin/com/back/BackApplicationTest.kt
+++ src/test/kotlin/com/back/BackApplicationTest.kt
@@ -2,9 +2,11 @@ package com.back
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-class Spring1ApplicationTests {
+@ActiveProfiles("test")
+class BackApplicationTest {
 
     @Test
     fun contextLoads() {
