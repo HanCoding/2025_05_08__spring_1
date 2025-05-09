39071cb2ede61f1779e9dd6e57f166571ec08f60
DeveloperRooney <han_0427@naver.com>
2025-05-09 16:09:31 +0900

    work

--- src/main/kotlin/com/back/domain/member/member/controller/ApiV1AdmMemberController.kt
+++ src/main/kotlin/com/back/domain/member/member/controller/ApiV1AdmMemberController.kt
@@ -1,10 +1,15 @@
 package com.back.domain.member.member.controller;
 
-import com.back.domain.member.member.dto.MemberDto
+import com.back.domain.member.member.dto.MemberWithUsernameDto
 import com.back.domain.member.member.service.MemberService
+import com.back.standard.extensions.getOrThrow
+import org.springframework.data.domain.Page
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope
 
 @RestController
 @RequestMapping("/api/v1/adm/members")
@@ -12,10 +17,21 @@ public class ApiV1AdmMemberController(
     private val memberService: MemberService
 ) {
 
-    @GetMapping("/me")
-    fun items(): List<MemberDto>{
-        return memberService.findAll().map {
-            MemberDto(it)
+    @GetMapping
+    fun items(
+        @RequestParam(defaultValue = "") searchKeyword: String,
+        @RequestParam(defaultValue = "1") page: Int,
+        @RequestParam(defaultValue = "30") pageSize: Int,
+    ): Page<MemberWithUsernameDto>{
+        return memberService.findAllByUsernameContaining(searchKeyword, page, pageSize).map {
+            MemberWithUsernameDto(it)
+        }
+    }
+
+    @GetMapping("/{id}")
+    fun item(@PathVariable id: Long): MemberWithUsernameDto{
+        return memberService.findById(id).getOrThrow().let{
+            MemberWithUsernameDto(it)
         }
     }
 }
